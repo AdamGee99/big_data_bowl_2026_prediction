44fb8286fa4e638adbde5f05b0b12cee9bebff9e
AdamGee99 <arg15@sfu.ca>
2025-10-15 12:54:06 -0700

    multi-player single play plot

--- data_cleaning_EDA.R
+++ data_cleaning_EDA.R
@@ -3,6 +3,9 @@
 library(tidyverse)
 library(here)
 library(data.table)
+library(gridExtra)
+library(patchwork)
+library(scales)
 
 
 ### Import Data ###
@@ -90,37 +93,75 @@ plot_player_movement = function(group_id) {
     filter(player_to_predict) %>% #filter for only players that were targeted
     group_by(game_id, nfl_id, play_id) %>% 
     filter(cur_group_id() == group_id) %>% #filter for a single player on a single play
-    select(frame_id, x, y, ball_land_x, ball_land_y, throw) %>%
+    select(frame_id, x, y, ball_land_x, ball_land_y, throw, player_side) %>%
     ungroup() %>% 
     ggplot(mapping = aes(x = x, y = y, colour = frame_id, shape = throw)) + #plot the movement
     geom_point(size = 3) +
     scale_colour_gradient(low = "black", high = "green") +
-    scale_shape_manual(values = c(19, 1)) +
+    scale_shape_manual(values = c(19, 1)) + #hollow is pre throw, filled is post throw
     geom_point(mapping = aes(x = ball_land_x, y = ball_land_y), colour = "red", size = 4) +
-    theme_bw()
+    theme_bw() +
+    guides(colour = "none", shape = "none")
 }
 
 plot_player_movement(group_id = 1)
 plot_player_movement(group_id = 2)
-plot_player_movement(group_id = 3)
-plot_player_movement(group_id = 4)
-plot_player_movement(group_id = 5)
-plot_player_movement(group_id = 6)
-plot_player_movement(group_id = 7)
-plot_player_movement(group_id = 8)
-plot_player_movement(group_id = 9)
+
+#plotting a grid 
+plots_1 = lapply(1:9, plot_player_movement)
+plots_2 = lapply(10:18, plot_player_movement)
+plots_3 = lapply(19:27, plot_player_movement)
+
+grid.arrange(grobs = plots_1, ncol = 3, nrow = 3)
+grid.arrange(grobs = plots_2, ncol = 3, nrow = 3)
+grid.arrange(grobs = plots_3, ncol = 3, nrow = 3)
+
+num_plots = 16
+grid.arrange(grobs = lapply(1:num_plots, plot_player_movement),
+             ncol = ceiling(sqrt(num_plots)),
+             nrow = ceiling(sqrt(num_plots)))
 
 
+#function now plotting multiple players on same play
+multi_player_movement = function(group_id) {
+  train %>% 
+    filter(player_to_predict) %>% #filter for only players that were targeted
+    group_by(game_id, play_id) %>% 
+    filter(cur_group_id() == group_id) %>% #filter for a single play
+    select(frame_id, x, y, ball_land_x, ball_land_y, throw, player_side, player_name) %>%
+    mutate(colour = ifelse(
+      player_side == "Offense", 
+      col_numeric(c("black", "green"), domain = range(frame_id))(frame_id),
+      col_numeric(c("black", "blue"), domain = range(frame_id))(frame_id)
+    )) %>% #add colours depending on offense or defense
+    ungroup() %>% 
+    ggplot(mapping = aes(x = x, y = y, colour = colour, shape = throw)) + #plot the movement
+    geom_point(size = 3) +
+    scale_colour_identity() + 
+    scale_shape_manual(values = c(19, 1)) + #hollow is pre throw, filled is post throw
+    geom_point(mapping = aes(x = ball_land_x, y = ball_land_y), colour = "red", size = 4) +
+    theme_bw() +
+    guides(shape = "none")
+}
+multi_player_movement(1)
 
+num_plots = 4
+wrap_plots(lapply(1:num_plots, multi_player_movement),
+           ncol = ceiling(sqrt(num_plots)),
+           nrow = ceiling(sqrt(num_plots))) +
+  plot_layout(guides = "collect")
+
+
+#filtering for all players in play gives interesting results too 
 
 
 ### TO DO ###
 
 
+
 #check if theres anything in test thats not in train... (any player for eg)
 
-
-
+#certain players are dominant to one side, eg always like to cut right, use this info?
 
 #add speed, direction, acceleration from predictions of model at every frame 
 
