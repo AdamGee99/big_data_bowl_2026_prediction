71848db47cd9d444bc169491807ce3a64f853166
AdamGee99 <arg15@sfu.ca>
2025-10-17 16:53:22 -0700

    correct coordinate system and more plots

--- baseline_model.R
+++ baseline_model.R
@@ -2,6 +2,7 @@
 
 library(tidyverse)
 library(here)
+library(ggrepel)
 
 ############################################### TO DO ############################################### 
 
@@ -41,36 +42,48 @@ lead_frames = 0
 lag_frames = 1
 window_size = lag_frames + lead_frames
 
-test = train %>%
+#add predictions to train
+train_pred = train %>%
   filter(player_to_predict) %>%
   group_by(game_id, nfl_id, play_id) %>%
-  filter(cur_group_id() == 1) %>% #use this to filter for only 1 player in 1 play in 1 game
+  #filter(cur_group_id() == group_id) %>% #use this to filter for only 1 player in 1 play in 1 game
   select(game_id, play_id, throw, s, a, dir, o, frame_id, player_name, x, y) %>%
-  ungroup() %>%
   #first calculate s, a, dir from true values of x,y after throw
-  mutate(x_diff = lead(x, n = lead_frames) - lag(x, n = lag_frames), #the difference in x direction from previous frame in yards
-         y_diff = lead(y, n = lead_frames) - lag(y, n = lag_frames), #the difference in y direction from previous frame in yards
+  mutate(x_diff = lead(x, n = lead_frames) - lag(x, n = lag_frames, default = NA), #the difference in x direction from previous frame in yards
+         y_diff = lead(y, n = lead_frames) - lag(y, n = lag_frames, default = NA), #the difference in y direction from previous frame in yards
          dist_diff = sqrt(x_diff^2 + y_diff^2), #distance travelled from previous frame in yards
          est_speed = dist_diff/((window_size)/10), #yards/second (1 frame is 0.1 seconds)
          est_acc_vector = (lead(est_speed, n = lead_frames) - lag(est_speed, n = lag_frames))/((window_size)/10), #this has directions (negative accelerations)
          est_acc_scalar = abs(est_acc_vector),
          est_dir = get_dir(x_diff = x_diff, y_diff = y_diff)) %>% 
+  ungroup() %>%
   #now calculate position in next frame using true s, a, dir
   mutate(pred_dist_diff = est_speed*0.1 + est_acc_vector*0.5*0.1^2, #using speed + acc
          pred_dist_diff_2 = est_speed*0.1, #using speed only
-         pred_x_sa = x + cos(est_dir*pi/180)*pred_dist_diff,
-         pred_y_sa = y + sin(est_dir*pi/180)*pred_dist_diff,
-         pred_x_s = x + cos(est_dir*pi/180)*pred_dist_diff_2,
-         pred_y_s = y + sin(est_dir*pi/180)*pred_dist_diff_2,
+         pred_x_sa = x + cos(((90 - est_dir) %% 360)*pi/180)*pred_dist_diff,
+         pred_y_sa = y + sin(((90 - est_dir) %% 360)*pi/180)*pred_dist_diff,
+         pred_x_s = x + cos(((90 - est_dir) %% 360)*pi/180)*pred_dist_diff_2,
+         pred_y_s = y + sin(((90 - est_dir) %% 360)*pi/180)*pred_dist_diff_2, #make sure you convert the angle back to original coordinate system, since using trig functions
   ) %>%
   #shift the predictions to the leading row (were predicting the next frame)
-  mutate(across(starts_with("pred_"), lag))
+  #this way were only using current/past data to predict future
+  group_by(game_id, nfl_id, play_id) %>%
+  mutate(across(starts_with("pred_"), lag)) %>%
+  ungroup()
 
 #' the predicted position from the next frame is the exact same movement from the previous frame
 #' that is, the player is going the same speed (travels the same distance) in the same direction
 
 #direction of previous frame vs direction of next frame
-ggplot(data = test, aes(x = est_dir, y = lead(est_dir))) +
+group_id = 1 #single player on single play
+plot_df = train_pred %>% 
+  group_by(game_id, nfl_id, play_id) %>% 
+  filter(cur_group_id() == group_id) %>%
+  ungroup() %>%
+  select(-c(game_id, nfl_id, play_id, o, player_name))
+
+#plot direction
+ggplot(data = plot_df, aes(x = est_dir, y = lead(est_dir))) +
   geom_point() +
   geom_abline(slope = 1, intercept = 0) +
   scale_x_continuous(limits = c(0, 360), breaks = c(0, 90, 180, 270, 360)) +
@@ -81,13 +94,13 @@ ggplot(data = test, aes(x = est_dir, y = lead(est_dir))) +
 #clearly this assumption doesn't make sense, this would mean player is travelling in straight line the whole time
 #same with speed and acceleration, they aren't the same throughout the play
 
-ggplot(data = test, aes(x = est_speed, y = lead(est_speed))) +
+ggplot(data = plot_df, aes(x = est_speed, y = lead(est_speed))) +
   geom_point() +
   geom_abline(slope = 1, intercept = 0) +
   labs(x = "Speed in Current Frame", y = "Speed in Next Frame") +
   theme_bw()
 
-ggplot(data = test, aes(x = est_acc_vector, y = lead(est_acc_vector))) +
+ggplot(data = plot_df, aes(x = est_acc_vector, y = lead(est_acc_vector))) +
   geom_point() +
   geom_abline(slope = 1, intercept = 0) +
   labs(x = "Acceleration in Current Frame", y = "Acceleration in Next Frame") +
@@ -97,19 +110,31 @@ ggplot(data = test, aes(x = est_acc_vector, y = lead(est_acc_vector))) +
 #' lets see how well it can predict using the true x,y coordinates though
 
 #predicted position vs actual position
-ggplot(data = test, mapping = aes(x = x, y = y)) + #use lead since the predictions are for the next (leading) frame
-  geom_point(colour = "black") +
-  geom_point(mapping = aes(x = pred_x_sa, y = pred_y_sa), colour = "blue") +
-  geom_point(mapping = aes(x = pred_x_s, y = pred_y_s), colour = "red") + 
+plot_df %>% 
+  rename(pred_x = pred_x_s, pred_y = pred_y_s) %>%
+  pivot_longer(cols = c(x, y, pred_x, pred_y),
+               names_to = c("obs", ".value"),
+               names_pattern = "(pred_)?(.*)") %>%
+  mutate(obs = ifelse(obs == "", "Recorded", "Estimated")) %>%
+  ggplot(mapping = aes(x = x, y = y, colour = obs)) + 
+  geom_point() +
+  #geom_text_repel(aes(label = frame_id)) +
   scale_x_continuous(n.breaks = 20) +
   scale_y_continuous(n.breaks = 20) +
   theme_bw()
+  
+# 
+# 
+#   geom_point(colour = "black") +
+#   geom_point(mapping = aes(x = pred_x_sa, y = pred_y_sa), colour = "blue") +
+#   geom_point(mapping = aes(x = pred_x_s, y = pred_y_s), colour = "red") + 
+  
 
 #some minor differences between using s only vs s + a
 
 
 #get predictions post-throw
-test_preds = test %>% filter(throw == "post") %>% select(frame_id, x, y, pred_x_sa, pred_y_sa, pred_x_s, pred_y_s)
+test_preds = plot_df %>% filter(throw == "post") %>% select(frame_id, x, y, pred_x_sa, pred_y_sa, pred_x_s, pred_y_s)
 
 #speed + acceleration
 get_rmse(true_x = test_preds$x, true_y = test_preds$y, 
@@ -118,83 +143,41 @@ get_rmse(true_x = test_preds$x, true_y = test_preds$y,
 get_rmse(true_x = test_preds$x, true_y = test_preds$y, 
          pred_x = test_preds$pred_x_s, pred_y = test_preds$pred_y_s)
 
-#speed only is better, why?
-#0.53 rmse, why isn't this better?
 
 
 
+#summarise rmse across all players
+#compare if using acceleration is better or not
+train_pred_summary = train_pred %>% 
+  filter(throw == "post") %>%
+  group_by(game_id, nfl_id, play_id) %>%
+  summarise(rmse_s = get_rmse(true_x = x, true_y = y,
+                              pred_x = pred_x_s, pred_y = pred_y_s),
+            rmse_sa = get_rmse(true_x = x, true_y = y,
+                               pred_x = pred_x_sa, pred_y = pred_y_sa))
+
+#plot
+train_pred_summary %>% 
+  pivot_longer(cols = starts_with("rmse"), 
+               names_to = "calc",
+               values_to = "value") %>%
+  ggplot(mapping = aes(x = calc, y = value)) +
+  geom_boxplot() +
+  theme_bw()
 
 
+#overall using speed + acceleration gives tiny better rmse on average
+train_pred_summary$rmse_sa %>% mean()
+train_pred_summary$rmse_s %>% mean()
 
-# # do it in a for loop to make sure everything's working properly
-# # just use previous frame only
-# 
-# results = data.frame()
-# for (i in 2:length(test)) {
-#   if (i == 2) { #start by using first observation (true vals)
-#     curr_x = test$x[2]
-#     curr_y = test$y[2]
-#     prev_x = test$x[1]
-#     prev_y = test$y[1]
-#     
-#     #difference in x,y from previous frame
-#     x_diff = curr_x - prev_x
-#     y_diff = curr_y - prev_y
-#     dist_diff = sqrt(x_diff^2 + y_diff^2)
-#     prev_s = test$s[1]
-#     
-#     #metric calculations
-#     curr_s = dist_diff/0.1
-#     curr_acc_vector = curr_s - prev_s
-#     curr_dir = (90 - (atan2(y = y_diff, x = x_diff)*180/pi)) %% 360
-#     
-#     #predict next frame position
-#     pred_dist_diff = curr_s*0.1 + curr_acc_vector*0.5*0.1^2
-#     pred_x = curr_x + cos(curr_dir*pi/180)*pred_dist_diff
-#     pred_y = curr_y + sin(curr_dir*pi/180)*pred_dist_diff
-#     
-#     #store predicted values
-#     results = results %>% rbind(c(
-#       test$x[i], test$y[i], curr_x, curr_y, pred_x, pred_y, curr_s, curr_acc_vector, curr_dir
-#     ))
-#     
-#     #update values
-#     curr_x = pred_x
-#     curr_y = pred_y
-#     prev_x = curr_x
-#     prev_y = curr_y
-#     prev_s = curr_s
-#   }
-#   
-#   #difference in x,y from previous frame
-#   x_diff = curr_x - prev_x
-#   y_diff = curr_y - prev_y
-#   dist_diff = sqrt(x_diff^2 + y_diff^2)
-#   
-#   #metric calculations
-#   curr_s = dist_diff/0.1
-#   curr_acc_vector = curr_s - prev_s
-#   curr_dir = (90 - (atan2(y = y_diff, x = x_diff)*180/pi)) %% 360
-#   
-#   #predict next frame position
-#   pred_dist_diff = curr_s*0.1 + curr_acc_vector*0.5*0.1^2
-#   pred_x = curr_x + cos(curr_dir*pi/180)*pred_dist_diff
-#   pred_y = curr_y + sin(curr_dir*pi/180)*pred_dist_diff
-#   
-#   #store predicted values
-#   results = results %>% rbind(c(
-#     test$x[i], test$y[i], curr_x, curr_y, pred_x, pred_y, curr_s, curr_acc_vector, curr_dir
-#   ))
-#   
-#   #update values
-#   curr_x = pred_x
-#   curr_y = pred_y
-#   prev_x = curr_x
-#   prev_y = curr_y
-#   prev_s = curr_s
-# }
-# colnames(results) = c("true_x", "true_y", "curr_x", "curr_y", "pred_x", "pred_y", "curr_s", "curr_acc", "curr_dir")
-# results
+#rmse over entire dataset
+train_pred %>% 
+  filter(throw == "post") %>% 
+  summarise(rmse_s = get_rmse(true_x = x, true_y = y,
+                              pred_x = pred_x_s, pred_y = pred_y_s),
+            rmse_sa = get_rmse(true_x = x, true_y = y,
+                               pred_x = pred_x_sa, pred_y = pred_y_sa))
+
 
 
 
