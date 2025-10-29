4d0c02e6378f709dea1a470b00953bf714f3b59e
AdamGee99 <arg15@sfu.ca>
2025-10-29 14:25:19 -0700

    converting for loop prediction in parallel

--- baseline_model.R
+++ baseline_model.R
@@ -107,118 +107,146 @@ curr_test_pred = curr_test %>%
 #what if you fit the models only on post throw?
 #or maybe filter out the first few frames in each play
 
+
+#attempting to do this in parallel
+#with future and furrr package
+
+library(foreach)
+library(doParallel)
+
+# Set up cluster
+num_cores = 16
+cl = makeCluster(num_cores)
+registerDoParallel(cl)
+
 #for testing
-curr_test_pred = curr_test_pred[1:500,]
+#curr_test_pred = curr_test_pred[1:1000,]
 
 #storing restuls
-results_pred = list()
+#results_pred = list()
 
-#loop
+
+#these are what we should parallelize over 
+game_player_play_ids = curr_test_pred$game_player_play_id %>% unique()
+
 set.seed(1999)
-for (i in 1:nrow(curr_test_pred)) {
-  if(i %% 10000 == 0) {print(paste0(round(i/nrow(curr_test_pred), 2), " complete"))} #see progress
-  
-  curr_row = curr_test_pred[i,]
 
-  #initialize position as last observed values before throw
-  if (curr_row$throw == "pre") { 
-    #if last observation pre throw, predict next frame position using true observed kinematic values
-    pred_dist_diff = curr_row$est_speed*0.1 + curr_row$est_acc*0.5*0.1^2
+start = Sys.time()
+
+results = foreach(group_id = game_player_play_ids, .combine = rbind, .packages = c("tidyverse", "xgboost", "doParallel", "progressr")) %dopar% {
+  
+  #single player on single play
+  curr_game_player_play_group = curr_test_pred %>% filter(game_player_play_id == group_id)
+  
+  #inner_group_results = list()
+  
+  #loop through frames in play (not in parallel)
+  foreach(i = 1:nrow(curr_game_player_play_group), .combine = rbind) %do% {
+    curr_row = curr_game_player_play_group[i,]
     
-    pred_x = curr_row$x + cos(((90 - curr_row$est_dir) %% 360)*pi/180)*pred_dist_diff
-    pred_y = curr_row$y + sin(((90 - curr_row$est_dir) %% 360)*pi/180)*pred_dist_diff
+    #initialize position as last observed values before throw
+    if (curr_row$throw == "pre") { 
+      #if last observation pre throw, predict next frame position using true observed kinematic values
+      pred_dist_diff = curr_row$est_speed*0.1 + curr_row$est_acc*0.5*0.1^2
+      
+      pred_x = curr_row$x + cos(((90 - curr_row$est_dir) %% 360)*pi/180)*pred_dist_diff
+      pred_y = curr_row$y + sin(((90 - curr_row$est_dir) %% 360)*pi/180)*pred_dist_diff
+      
+      #predict dir, s, a
+      xg_pred_df = curr_row %>%  #row to predict on
+        select(all_of(dir_xg$feature_names))
+      
+      pred_dir_diff = predict(dir_xg, data.matrix(xg_pred_df))
+      pred_s_diff = predict(speed_xg, data.matrix(xg_pred_df))
+      pred_a_diff = predict(acc_xg, data.matrix(xg_pred_df))
+      
+      #predicted dir, s, a
+      pred_dir = curr_row$pred_dir = curr_row$est_dir + pred_dir_diff
+      pred_s = curr_row$pred_s = curr_row$est_speed + pred_s_diff
+      pred_a = curr_row$pred_a = curr_row$est_acc + pred_a_diff
+      
+      #### Do we predict x,y first, then predict dir, s, a?,
+      # if so then we can actually use the predicted x,y as a feature in the dir, s, a model...
+      # problem is when we use the true future x,y to train, it might b too good and much better than our predictions
+      # making model say the leading x,y is more important than it actually is...
+      
+      ## could also do this the other way around too, predict the future dir, s, a then predict future x,y
+      
+      
+      # maybe theres a way to do this simultaneously...? like do it both ways, then take the average of each prediction?
+      
+    } else {
+      prev_row = inner_results_pred
+      
+      #first predict kinematics at current frame using previous frame's predicted position
+      #set current position and dir, s, a as previous prediction 
+      curr_row$x = prev_row$pred_x
+      curr_row$y = prev_row$pred_y
+      #prev_x = prev_row$x
+      #prev_y = prev_row$y
+      
+      #current dir, s, a
+      curr_row$est_dir = prev_row$pred_dir
+      curr_row$est_speed = prev_row$pred_s 
+      curr_row$est_acc = prev_row$pred_a
+      
+      ### predict kinematics using xg models
+      #update all features necessary for dir,s,a models
+      
+      prev_curr_frame_df = prev_row %>%
+        select(-c(starts_with("pred_"))) %>% #remove the pred columns so we can bind
+        rbind(curr_row)
+      
+      # now get change in kinematics and derived features
+      prev_curr_frame_df = prev_curr_frame_df %>%
+        change_in_kinematics() %>%
+        derived_features() %>%
+        mutate(prev_x_diff = x - lag(x),
+               prev_y_diff = y - lag(y))
+      
+      #manually update prop_play_complete
+      prev_curr_frame_df$prop_play_complete[2] = curr_row$prop_play_complete
+      
+      #predict dir, s, a
+      xg_pred_df = prev_curr_frame_df[2,] %>%  #row to predict on
+        select(all_of(dir_xg$feature_names))
+      
+      pred_dir_diff = predict(dir_xg, data.matrix(xg_pred_df))
+      pred_s_diff = predict(speed_xg, data.matrix(xg_pred_df))
+      pred_a_diff = predict(acc_xg, data.matrix(xg_pred_df))
+      
+      pred_dir = curr_row$est_dir + pred_dir_diff
+      pred_s = curr_row$est_speed + pred_s_diff
+      pred_a = curr_row$est_acc + pred_a_diff
+      
+      #update the remaining features that rely on predicted dir, s, a
+      
+      #finally predict next frame position
+      pred_dist_diff = pred_s*0.1 + pred_a*0.5*0.1^2
+      
+      pred_x = curr_row$x + cos(((90 - pred_dir) %% 360)*pi/180)*pred_dist_diff
+      pred_y = curr_row$y + sin(((90 - pred_dir) %% 360)*pi/180)*pred_dist_diff
+    }
     
-    #predict dir, s, a
-    xg_pred_df = curr_row %>%  #row to predict on
-      select(all_of(dir_xg$feature_names))
+    #store predicted positions
+    curr_row$pred_x = pred_x
+    curr_row$pred_y = pred_y
     
-    pred_dir_diff = predict(dir_xg, data.matrix(xg_pred_df))
-    pred_s_diff = predict(speed_xg, data.matrix(xg_pred_df))
-    pred_a_diff = predict(acc_xg, data.matrix(xg_pred_df))
+    #store predicted kinematics
+    curr_row$pred_dir = pred_dir
+    curr_row$pred_s = pred_s
+    curr_row$pred_a = pred_a
     
-    #predicted dir, s, a
-    pred_dir = curr_row$pred_dir = curr_row$est_dir + pred_dir_diff
-    pred_s = curr_row$pred_s = curr_row$est_speed + pred_s_diff
-    pred_a = curr_row$pred_a = curr_row$est_acc + pred_a_diff
-    
-    
-    #### Do we predict x,y first, then predict dir, s, a?,
-    # if so then we can actually use the predicted x,y as a feature in the dir, s, a model...
-    # problem is when we use the true future x,y to train, it might b too good and much better than our predictions
-    # making model say the leading x,y is more important than it actually is...
-    
-    ## could also do this the other way around too, predict the future dir, s, a then predict future x,y
-    
-    
-    # maybe theres a way to do this simultaneously...? like do it both ways, then take the average of each prediction?
-    
-    
-  } else {
-    prev_row = results_pred[[i-1]]
-    
-    #first predict kinematics at current frame using previous frame's predicted position
-    #set current position and dir, s, a as previous prediction 
-    curr_row$x = prev_row$pred_x
-    curr_row$y = prev_row$pred_y
-    #prev_x = prev_row$x
-    #prev_y = prev_row$y
-    
-    #current dir, s, a
-    curr_row$est_dir = prev_row$pred_dir
-    curr_row$est_speed = prev_row$pred_s 
-    curr_row$est_acc = prev_row$pred_a
-    
-    ### predict kinematics using xg models
-    #update all features necessary for dir,s,a models
-    
-    prev_curr_frame_df = prev_row %>%
-      select(-c(starts_with("pred_"))) %>% #remove the pred columns so we can bind
-      rbind(curr_row)
-    
-    # now get change in kinematics and derived features
-    prev_curr_frame_df = prev_curr_frame_df %>%
-      change_in_kinematics() %>%
-      derived_features() %>%
-      mutate(prev_x_diff = x - lag(x),
-             prev_y_diff = y - lag(y))
-    
-    #manually update prop_play_complete
-    prev_curr_frame_df$prop_play_complete[2] = curr_row$prop_play_complete
-    
-    #predict dir, s, a
-    xg_pred_df = prev_curr_frame_df[2,] %>%  #row to predict on
-      select(all_of(dir_xg$feature_names))
-    
-    pred_dir_diff = predict(dir_xg, data.matrix(xg_pred_df))
-    pred_s_diff = predict(speed_xg, data.matrix(xg_pred_df))
-    pred_a_diff = predict(acc_xg, data.matrix(xg_pred_df))
-    
-    pred_dir = curr_row$est_dir + pred_dir_diff
-    pred_s = curr_row$est_speed + pred_s_diff
-    pred_a = curr_row$est_acc + pred_a_diff
-    
-    #update the remaining features that rely on predicted dir, s, a
-    
-    #finally predict next frame position
-    pred_dist_diff = pred_s*0.1 + pred_a*0.5*0.1^2
-    
-    pred_x = curr_row$x + cos(((90 - pred_dir) %% 360)*pi/180)*pred_dist_diff
-    pred_y = curr_row$y + sin(((90 - pred_dir) %% 360)*pi/180)*pred_dist_diff
+    #return result
+    inner_results_pred = curr_row
+    inner_results_pred
   }
-  
-  #store predicted positions
-  curr_row$pred_x = pred_x
-  curr_row$pred_y = pred_y
-  
-  #store predicted kinematics
-  curr_row$pred_dir = pred_dir
-  curr_row$pred_s = pred_s
-  curr_row$pred_a = pred_a
-  
-  #store
-  results_pred[[i]] = curr_row
 }
-#def need to do this in parallel
+end = Sys.time()
+end - start
+results
+
+
 
 
 ### experiment with this - figure out the best method - compare rmse at the end
@@ -241,14 +269,14 @@ true_x_y = train %>%
   select(game_player_play_id, frame_id, true_x, true_y)
 
 #bind results into df
-results_pred = results_pred %>%
+results_pred = results %>%
   bind_rows() %>%
   left_join(true_x_y, by = c("game_player_play_id", "frame_id")) #join true x,y values
  
 
 
 
-group_id = 178
+group_id = 1
 
 results_pred_single_play = results_pred %>% 
   group_by(game_player_play_id) %>%
@@ -262,7 +290,7 @@ plot_player_movement_pred(group_id = unique(results_pred_single_play$game_player
                           group_id_preds = results_pred_single_play %>% select(frame_id, pred_x, pred_y))
 
 
-group_id = 20
+group_id = 50
 #now plot multiple players on same play with predictions
 multi_player_pred_single_play = results_pred %>% 
   group_by(game_play_id) %>%
@@ -307,8 +335,6 @@ results_pred %>%
   filter(throw == "post") %>%
   summarise(rmse = get_rmse(true_x = true_x, true_y = true_y,
                             pred_x = x, pred_y = y))
-
-#0.94
 
 #boxplot of distance of predicted vs true of each frame across all players, games, plays
 pred_error = results_pred %>% 
