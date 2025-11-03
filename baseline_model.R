33eb6196e7feffeb9affd4cbab23e6b0f287c6aa
AdamGee99 <arg15@sfu.ca>
2025-11-03 15:51:54 -0800

    filter out start of play for fitting

--- baseline_model.R
+++ baseline_model.R
@@ -48,6 +48,10 @@ data_mod = train %>%
   select(-c(game_id, nfl_id, play_id, o, player_to_predict, s, a, dir,
           player_birth_date, num_frames_output, num_frames))
 
+#what proportion of play being complete is ball thrown
+data_mod %>% filter(throw == "post" & lag(throw) == "pre") %>% pull(prop_play_complete) %>% hist()
+
+#min is 0.225 - fit models mostly on post-throw
 
 #these have the true x,y values but that's ok
 #fitting it on true value calculations is ok, but we just can't use the true values to calculate when we actually predict
@@ -65,10 +69,11 @@ curr_train[,1:3] %>% summary()
 curr_train$fut_s_diff %>% quantile(probs = c(0.001, 0.999), na.rm = TRUE)
 curr_train$fut_a_diff %>% quantile(probs = c(0.001, 0.999), na.rm = TRUE)
 curr_train = data_mod %>% filter(abs(fut_s_diff) <= 1,
-                               abs(fut_a_diff) <= 11)
+                               abs(fut_a_diff) <= 11) %>%
+  filter(throw == "post") #play is more than 10% complete
+
 
 #fit models
-
 #remove all the player-specific stuff for now, just focus on kinematics and ball landing features
 xg_train_df = curr_train %>%
   select(-c(game_player_play_id, game_play_id, frame_id, x, y,
@@ -100,13 +105,12 @@ curr_test_pred = curr_test %>%
   filter(throw == "post" | (throw == "pre" & lead(throw) == "post")) %>% #filter for post throw only
          #mutate the unknowns to be NA to ensure the model isn't using any future known data
          mutate(across(-c(game_player_play_id, game_play_id, throw, frame_id, play_direction, 
-                   absolute_yardline_number, player_name, player_height, player_weight, 
+                   absolute_yardline_number, player_name, player_height, player_weight, player_position,
                    player_side, player_role, ball_land_x, ball_land_y, prop_play_complete), 
                 ~ ifelse(throw == "pre", .x, NA))) 
 
 #what if you fit the models only on post throw?
 #or maybe filter out the first few frames in each play
-
 
 #attempting to do this in parallel
 #with future and furrr package
@@ -115,16 +119,17 @@ library(foreach)
 library(doParallel)
 
 # Set up cluster
-num_cores = parallel::detectCores() - 4
+num_cores = parallel::detectCores() - 2
 cl = makeCluster(num_cores)
 registerDoParallel(cl)
 
 #for testing
-#curr_test_pred = curr_test_pred[1:500,]
+#curr_test_pred = curr_test_pred[1:1000,]
 
 #these are what we should parallelize over 
 game_player_play_ids = curr_test_pred$game_player_play_id %>% unique()
 
+#this is my predict() function
 
 set.seed(1999)
 start = Sys.time()
@@ -145,28 +150,19 @@ results = foreach(group_id = game_player_play_ids, .combine = rbind, .packages =
       pred_x = curr_row$x + cos(((90 - curr_row$est_dir) %% 360)*pi/180)*pred_dist_diff
       pred_y = curr_row$y + sin(((90 - curr_row$est_dir) %% 360)*pi/180)*pred_dist_diff
       
-      #predict dir, s, a
+      #predict future dir, s, a (next frame)
       xg_pred_df = curr_row %>%  #row to predict on
         select(all_of(dir_xg$feature_names))
       
-      pred_dir_diff = predict(dir_xg, data.matrix(xg_pred_df))
-      pred_s_diff = predict(speed_xg, data.matrix(xg_pred_df))
-      pred_a_diff = predict(acc_xg, data.matrix(xg_pred_df))
+      fut_dir_diff = predict(dir_xg, data.matrix(xg_pred_df))
+      fut_s_diff = predict(speed_xg, data.matrix(xg_pred_df))
+      fut_a_diff = predict(acc_xg, data.matrix(xg_pred_df))
       
-      #predicted dir, s, a
-      pred_dir = curr_row$pred_dir = curr_row$est_dir + pred_dir_diff
-      pred_s = curr_row$pred_s = curr_row$est_speed + pred_s_diff
-      pred_a = curr_row$pred_a = curr_row$est_acc + pred_a_diff
+      #predicted dir, s, a using xg models
+      pred_dir = curr_row$pred_dir = curr_row$est_dir + fut_dir_diff
+      pred_s = curr_row$pred_s = curr_row$est_speed + fut_s_diff
+      pred_a = curr_row$pred_a = curr_row$est_acc + fut_a_diff
       
-      #### Do we predict x,y first, then predict dir, s, a?,
-      # if so then we can actually use the predicted x,y as a feature in the dir, s, a model...
-      # problem is when we use the true future x,y to train, it might b too good and much better than our predictions
-      # making model say the leading x,y is more important than it actually is...
-      
-      ## could also do this the other way around too, predict the future dir, s, a then predict future x,y
-      
-      
-      # maybe theres a way to do this simultaneously...? like do it both ways, then take the average of each prediction?
       
     } else {
       prev_row = inner_results_pred
@@ -180,7 +176,7 @@ results = foreach(group_id = game_player_play_ids, .combine = rbind, .packages =
       curr_row$est_speed = prev_row$pred_s 
       curr_row$est_acc = prev_row$pred_a
       
-      ### predict kinematics using xg models
+      ### predict future kinematics using xg models
       #update all features necessary for dir,s,a models
       prev_curr_frame_df = prev_row %>%
         select(-c(starts_with("pred_"))) %>% #remove the pred columns so we can bind
@@ -200,21 +196,19 @@ results = foreach(group_id = game_player_play_ids, .combine = rbind, .packages =
       xg_pred_df = prev_curr_frame_df[2,] %>%  #row to predict on
         select(all_of(dir_xg$feature_names))
       
-      pred_dir_diff = predict(dir_xg, data.matrix(xg_pred_df))
-      pred_s_diff = predict(speed_xg, data.matrix(xg_pred_df))
-      pred_a_diff = predict(acc_xg, data.matrix(xg_pred_df))
+      fut_dir_diff = predict(dir_xg, data.matrix(xg_pred_df))
+      fut_s_diff = predict(speed_xg, data.matrix(xg_pred_df))
+      fut_a_diff = predict(acc_xg, data.matrix(xg_pred_df))
       
-      pred_dir = curr_row$est_dir + pred_dir_diff
-      pred_s = curr_row$est_speed + pred_s_diff
-      pred_a = curr_row$est_acc + pred_a_diff
+      #predicted dir, s, a
+      pred_dir = curr_row$pred_dir = curr_row$est_dir + fut_dir_diff
+      pred_s = curr_row$pred_s = curr_row$est_speed + fut_s_diff
+      pred_a = curr_row$pred_a = curr_row$est_acc + fut_a_diff
       
-      #update the remaining features that rely on predicted dir, s, a
-      
-      #finally predict next frame position
-      pred_dist_diff = pred_s*0.1 + pred_a*0.5*0.1^2
-      
-      pred_x = curr_row$x + cos(((90 - pred_dir) %% 360)*pi/180)*pred_dist_diff
-      pred_y = curr_row$y + sin(((90 - pred_dir) %% 360)*pi/180)*pred_dist_diff
+      #predict x,y
+      pred_dist_diff = curr_row$est_speed*0.1 + curr_row$est_acc*0.5*0.1^2
+      pred_x = curr_row$x + cos(((90 - curr_row$est_dir) %% 360)*pi/180)*pred_dist_diff
+      pred_y = curr_row$y + sin(((90 - curr_row$est_dir) %% 360)*pi/180)*pred_dist_diff
     }
     
     #store predicted positions
@@ -233,9 +227,7 @@ results = foreach(group_id = game_player_play_ids, .combine = rbind, .packages =
 }
 end = Sys.time()
 end - start
-results
-
-
+#results
 
 ### experiment with this - figure out the best method - compare rmse at the end
 #' 1. predict x,y first, then use future x,y to predict future dir, s, a
@@ -249,22 +241,29 @@ results
 #' 
 #' can keep doing this until convergence... 
 #' but how computationally feasible is this
+#' 
+#' 
 
-#the true x,y values
-true_x_y = train %>%
+
+
+
+#the true x,y,dir,s,a values
+true_vals = train %>%
   filter(!(game_player_play_id %in% split)) %>% 
-  rename(true_x = x, true_y = y) %>% 
-  select(game_player_play_id, frame_id, true_x, true_y)
+  rename(true_dir = est_dir, true_s = est_speed, true_a = est_acc, true_x = x, true_y = y) %>% 
+  select(game_player_play_id, frame_id, true_dir, true_s, true_a, true_x, true_y)
 
 #bind results into df
 results_pred = results %>%
   bind_rows() %>%
-  left_join(true_x_y, by = c("game_player_play_id", "frame_id")) #join true x,y values
- 
+  left_join(true_vals, by = c("game_player_play_id", "frame_id")) #join true x,y values
 
 
-#singl player
-group_id = 2
+#pred dir, s, a vs true dir, s, a
+group_id = 10
+dir_s_a_eval(group_id)
+
+#single player movement
 curr_game_player_play_id = results_pred %>% 
   group_by(game_player_play_id) %>%
   filter(cur_group_id() == group_id) %>% 
@@ -273,10 +272,12 @@ curr_game_player_play_id = results_pred %>%
 plot_player_movement_pred(group_id = curr_game_player_play_id,
                           group_id_preds = results_pred %>% 
                             filter(game_player_play_id == curr_game_player_play_id) %>%
-                            select(frame_id, pred_x, pred_y))
+                            select(frame_id, x, y) %>%
+                            rename(pred_x = x, pred_y = y))
 
-#mulitple players on play
-group_id = 4
+
+#multiple players on play
+group_id = 1
 curr_game_play_id = results_pred %>% 
   group_by(game_play_id) %>%
   filter(cur_group_id() == group_id) %>% 
@@ -285,7 +286,8 @@ curr_game_play_id = results_pred %>%
 multi_player_movement_pred(group_id = curr_game_play_id,
                            group_id_preds = results_pred %>%
                              filter(game_play_id == curr_game_play_id) %>%
-                             select(game_player_play_id, frame_id, pred_x, pred_y))
+                             select(game_player_play_id, frame_id, x, y) %>%
+                             rename(pred_x = x, pred_y = y))
 
 #lots of players dont have predictions since they werent in the test set
 
@@ -313,6 +315,11 @@ results_pred %>%
   filter(throw == "post") %>%
   summarise(rmse = get_rmse(true_x = true_x, true_y = true_y,
                             pred_x = x, pred_y = y))
+
+#fit on all -                     - 1.111
+#fit on prop_play_complete > 0.4  - 1.083
+#fit on throw == "post"           - 1.154
+
 
 
 
